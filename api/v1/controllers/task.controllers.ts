ff0ffbb43452704102bbc969e56c5980cdadcac4
CodeMnh <minhkhac1002@gmail.com>
2025-04-08 15:09:44 +0700

    Tính năng sắp xếp theo trạng thái

--- api/v1/controllers/task.controllers.ts
+++ api/v1/controllers/task.controllers.ts
@@ -5,10 +5,15 @@ export const index = async (req: Request, res: Response) => {
     let find = {
         deleted : false
     }
+    // Bộ lọc theo trạng thái
     if(req.query.status){
         find["status"] = req.query.status;
     }
-    const tasks = await Task.find(find)
+    // Sắp xếp theo tiêu chí 
+    let sort = {};
+    sort[req.query.sortKey.toString()] = req.query.sortValue.toString();
+
+    const tasks = await Task.find(find).sort(sort);
     res.json(tasks)
 }
 
