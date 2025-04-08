7bf8c711332feb441b1d8c66521904040b4f667d
CodeMnh <minhkhac1002@gmail.com>
2025-04-08 14:32:30 +0700

    Cấu trúc thư mục theo mô hình mvc tiêp

--- api/v1/routes/taks.router.ts
+++ api/v1/routes/taks.router.ts
@@ -1,23 +1,9 @@
 import { Router, Request, Response } from "express"
 const router: Router = Router()
-import Task from "../models/task.model"
+import * as controller from "../controllers/task.controllers"
 
-router.get('/', async (req: Request, res: Response) => {
-    const tasks = await Task.find({
-        deleted : false
-    })
-    console.log(tasks);
-    res.json(tasks)
-})
+router.get('/', controller.index)
 
-router.get('/detail/:id', async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const task = await Task.findOne({
-        _id: id,
-        deleted : false
-    })
-    console.log(task);
-    res.json(task)
-})
+router.get('/detail/:id', controller.detail)
 
 export const TasksRouter = router
